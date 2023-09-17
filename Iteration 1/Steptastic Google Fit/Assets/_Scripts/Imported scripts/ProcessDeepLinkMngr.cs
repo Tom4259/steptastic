c73ce8f8f76a9cef57945d73d59d4fc990ba8c52
Tom4259 <tomredway1@gmail.com>
2023-09-17 11:16:14 +0100

    Changed to Fitbit integration
    Haven't yet saved the user details to device.

--- Iteration 1/Steptastic Google Fit/Assets/_Scripts/Imported scripts/ProcessDeepLinkMngr.cs
+++ Iteration 1/Steptastic Google Fit/Assets/_Scripts/Imported scripts/ProcessDeepLinkMngr.cs
@@ -10,24 +10,21 @@ public class ProcessDeepLinkMngr : MonoBehaviour
 {
     public static ProcessDeepLinkMngr Instance { get; private set; }
     public string deeplinkURL = "unitydl://Steptastic";
-
-
-
-    private string clientID = "452921919955-6lqch4h3ii4u9dgckneq9t13u6muf21b.apps.googleusercontent.com";
-    //private string clientSecret = "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";
-
-
     [TextArea]
-    public string scopes = "https://www.googleapis.com/auth/fitness.activity.read";
+    public string scopes = "activity%20profile%20settings";
 
+    [Space]
+    [TextArea]
+    public string editorAuthToken = "";
 
-    private string authURL = "https://accounts.google.com/o/oauth2/v2/auth";
+    private string authURL = "https://www.fitbit.com/oauth2/authorize?";
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+
             Application.deepLinkActivated += onDeepLinkActivated;
             if (!String.IsNullOrEmpty(Application.absoluteURL))
             {
@@ -44,19 +41,16 @@ public class ProcessDeepLinkMngr : MonoBehaviour
         }
     }
 
-    public void startLoginToGoogle()
+    public void startLoginToFitbit()
     {
-        Application.OpenURL(authURL + "?client_id=" + clientID + "&redirect_uri=unitydl://steptastic&response_type=code&scope=" + scopes);        
+        //add an editor token, so don't need to keep logging in to fitbit account on run
+        Application.OpenURL(authURL + "response_type=token&client_id=22C4ND&redirect_uri=unitydl%3A%2F%2FSteptastic&scope=" + scopes + "&expires_in=31536000");
     }
 
     public void onDeepLinkActivated(string url)
     {
         // Update DeepLink Manager global variable, so URL can be accessed from anywhere.
         deeplinkURL = url;
-
-        // Decode the URL to determine action. 
-        // In this example, the app expects a link formatted like this:
-        // unitydl://mylink?scene1 
         #region splitting returned data
 
         string[] returnedUrl = url.Split('&');
@@ -64,22 +58,10 @@ public class ProcessDeepLinkMngr : MonoBehaviour
         string user_id = returnedUrl[1].Split('=')[1];
         int expires_in = int.Parse(returnedUrl[returnedUrl.Length - 1].Split('=')[1]);
 
-        //Debug.Log("access token: " + access_token);
-        //Debug.Log("user id: " + user_id);
-        //Debug.Log("expires in: " + expires_in);
+        Debug.Log("access token: " + access_token);
+        Debug.Log("user id: " + user_id);
+        Debug.Log("expires in: " + expires_in);
 
         #endregion
-
-        #region saving data
-
-        //PlayerPrefsX.SetString(PlayerPrefsKeyStorage.Fitbit.fitbitAccessToken, access_token);
-        //PlayerPrefsX.SetString(PlayerPrefsKeyStorage.Fitbit.fitbitUserID, user_id);
-        //PlayerPrefsX.SetInt(PlayerPrefsKeyStorage.Fitbit.fitbitAccessTokenExpires, expires_in);
-
-        PlayerPrefs.Save();
-
-        #endregion
-
-        //UserSetupManager.instance.userSuccessfullyLoggedIntoFitbit();
     }
 }
