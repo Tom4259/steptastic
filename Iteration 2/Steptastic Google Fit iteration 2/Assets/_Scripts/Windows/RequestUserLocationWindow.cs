677667c543f993338fa3842242f1aa69c52394a5
Tom4259 <tomredway1@gmail.com>
2023-10-08 22:02:21 +0100

    Removed AndroidRuntimePermissions
    As no longer needed/used

--- Iteration 2/Steptastic Google Fit iteration 2/Assets/_Scripts/Windows/RequestUserLocationWindow.cs
+++ Iteration 2/Steptastic Google Fit iteration 2/Assets/_Scripts/Windows/RequestUserLocationWindow.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AndroidRuntimePermissionsNamespace;
 using Michsky.MUIP;
 using UnityEngine.UI;
-using Sisus.Debugging.Console;
 #if !UNITY_EDITOR
 //using Debug = Sisus.Debugging.Debug;
 #endif
