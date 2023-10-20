56a324270b14029af7a3a50f0f3740c0f3b6e8bd
Tom4259 <tomredway1@gmail.com>
2023-10-20 19:21:11 +0100

    Dark mode iOS and Android plugins for dark mode

--- Iteration 3/Steptastic Google Fit iteration 3/Assets/_Scripts/UIThemeManager.cs
+++ Iteration 3/Steptastic Google Fit iteration 3/Assets/_Scripts/UIThemeManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class UIThemeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+#if UNITY_EDITOR
+
+    public bool isDarkMode = false;
+
+    public bool IsDarkMode()
     {
-        
+        return isDarkMode;
     }
 
-    // Update is called once per frame
-    void Update()
+
+#elif UNITY_ANDROID && !UNITY_EDITOR                               
+
+    private AndroidJavaObject darkModeHelper;
+
+    private void Start()
     {
-        
+        darkModeHelper = new AndroidJavaObject("com.TomIndustries.Steptastic.DarkModeHelper");
+
+        Debug.Log(IsDarkMode());
     }
+
+    public bool IsDarkMode()
+    {
+        if (darkModeHelper != null)
+        {
+            return darkModeHelper.CallStatic<bool>("isDarkMode", new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"));
+        }
+        else
+        {
+            Debug.LogError("DarkModeHelper not initialized.");
+            return false;
+        }
+    }
+
+#elif UNITY_IOS && !UNITY_EDITOR
+
+    [DllImport("__Internal")]
+    private static extern bool IsDarkModeEnabled();
+
+    private void Start()
+    {
+        bool isDarkMode = IsDarkModeEnabled();
+        Debug.Log("Dark Mode is enabled: " + isDarkMode);
+    }
+
+    public bool IsDarkMode()
+    {
+        return IsDarkModeEnabled();
+    }
+
+#endif
 }
